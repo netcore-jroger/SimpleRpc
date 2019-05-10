d7871cc1b0c37190db41111e9952f58b33d07060
JRoger <sjr_vip@126.com>
2019-05-10 08:41:31 +0800

    update client sample.

--- sample/ClientSample/Program.cs
+++ sample/ClientSample/Program.cs
@@ -20,26 +20,26 @@ namespace ClientSample
                 .AddRpcClientService<IUserService>()
                 .BuildServiceProvider();
 
-            var userService = provider.GetService<IUserService>();
-            var userRequest = new UserRequest {
-                Id = 1
-            };
+                var userRequest = new UserRequest {
+                    Id = 1
+                };
 
-            while (true)
-            {
-                var tokenSource = new CancellationTokenSource(1000 * 50);
-                Console.Write("Please input keyword:");
-                var input = Console.ReadLine();
-                if (input.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                while (true)
                 {
-                    break;
+                    Console.Write("Please input keyword:");
+                    var input = Console.ReadLine();
+                    if (input.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
+                    userRequest.Keyword = input;
+                    var tokenSource = new CancellationTokenSource(1000 * 10);
+                    var userService = provider.GetService<IUserService>();
+                    var userDto = await userService.GetUserBy(userRequest, tokenSource.Token);
+
+                    Console.WriteLine($"Id: {userDto.Id}, Name: {userDto.Name}, CreateDate: {userDto.CreateDate:yyyy-MM-dd HH:mm:ss fff}");
                 }
-
-                userRequest.Keyword = input;
-                var userDto = await userService.GetUserBy(userRequest, tokenSource.Token);
-
-                Console.WriteLine($"Id: {userDto.Id}, Name: {userDto.Name}, CreateDate: {userDto.CreateDate:yyyy-MM-dd HH:mm:ss fff}");
-            }
 
             Console.WriteLine("press any key to exit.");
             Console.ReadKey(true);
