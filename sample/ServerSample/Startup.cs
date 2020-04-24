457fbeaa4cf7c10e3ec8761eb803705fe998ca54
JRoger <sjr_vip@126.com>
2020-04-24 16:41:53 +0800

    chore: upgrade project to .net core 3.1

--- sample/ServerSample/Startup.cs
+++ sample/ServerSample/Startup.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using InterfaceLib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using InterfaceLib;
 using SimpleRpc.Server;
 using SimpleRpc.Shared;
 
@@ -24,7 +25,7 @@ namespace ServerSample
             services.AddScoped<IUserService, UserService>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
